b1f0ba88159230b5529320db23b494615d7f2a81
cc0a <jmckeeco84@gmail.com>
2020-10-31 02:26:15 -0600

    init commit

---
+++ variables.cpp
@@ -0,0 +1,35 @@
+// Your First C++ Program
+#include <iostream>
+
+using namespace std;
+//Variables
+
+int main()
+{
+    int a = 40, b = 53, c = 20;
+
+    cout << "a : " << a << " address: " << &a << endl;
+    cout << "b : " << b << " address: " << &b << endl;
+    cout << "c : " << c << " address: " << &c << endl;
+
+    string t5 = "Hello World!";
+
+    cout << t5 << endl;
+
+    string x = "part1";
+    string y = " part2";
+    string combinedStrings = x + y;
+
+    cout << combinedStrings << endl;
+
+    bool t6 = false;
+
+    cout << t6 << endl;
+
+    unsigned short int t7 = 3700;
+    cout << t7 << endl;
+}
+
+
+
+
